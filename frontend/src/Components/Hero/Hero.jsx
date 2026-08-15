9bbdf855e08ddfc912f9fd78f52660c1a1b41279
abhi23333 <161305604+abhi23333@users.noreply.github.com>
2026-08-15 16:47:23 +0530

    Update Hero.jsx

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
-import arrow_icon from '../Assets/arrow.png'
 import hero_image from '../Assets/iphone.png'
 
 const Hero = () => {
