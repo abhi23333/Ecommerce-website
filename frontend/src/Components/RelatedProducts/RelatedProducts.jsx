bfbb075d0eaed419ffa87730b35a7a8505702bf7
abhi23333 <161305604+abhi23333@users.noreply.github.com>
2026-08-15 16:48:56 +0530

    Update RelatedProducts.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -5,24 +5,33 @@ import Item from '../Item/Item'
 const RelatedProducts = (props) => {
 
   const [relatedProducts, setRelatedProducts] = useState([]);
-  const {product} = props;
+  const { product } = props;
 
   useEffect(() => {
     console.log(product)
-    // var category = props.product.category;
-    // console.log(category.toLowerCase());
+
     fetch(`http://localhost:4000/related-${product.category}`)
-    .then(res => res.json())
-    .then(data => setRelatedProducts(data));
-  },[])
+      .then(res => res.json())
+      .then(data => setRelatedProducts(data));
+  }, [product])
 
   return (
     <div className='relatedproducts'>
       <h1>Related Products</h1>
       <hr />
+
       <div className="relatedproducts-item">
-        {relatedProducts.map((item,i) => {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+        {relatedProducts.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          )
         })}
       </div>
     </div>
